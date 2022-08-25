f69b066db22bad69ae0934b262bae08027b849fc
Cecilia Wu <fangwu.cecilia@gmail.com>
2022-08-25 14:20:58 +0100

    chatroom

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :chatrooms, only: :show do
+    resources :messages, only: :create
+  end
+  # Deresources :chatrooms, only: :showfine your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
